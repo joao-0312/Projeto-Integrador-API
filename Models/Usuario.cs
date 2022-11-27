63e63366fd85897576d7a28ec7fc24fb4a9e6671
joao <joaoeifler2004@gmail.com>
2022-11-27 17:34:05 -0400

    Adicionados mais dados ao model de usuário

--- Models/Usuario.cs
+++ Models/Usuario.cs
@@ -2,9 +2,14 @@ namespace Projeto_Integrador_API.Models
 {
     public class Usuario
     {
-        public int UsuarioID { get; set; }
+        public int Id { get; set; }
         public string Nome { get; set; }
+        public string CPF { get; set; }
         public DateTime DataNascimento { get; set; }
+        public string Telefone { get; set; }
         public string Genero { get; set; }
+        public string Email { get; set; }
+        public string Senha { get; set; }
+        public Endereco Endereco { get; set; }
     }
 }
